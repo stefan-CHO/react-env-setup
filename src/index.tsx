a668a04e40b9b0b13d37379609f119ff088e0ea5
skku-sungjin <skku7714@gmail.com>
2020-04-05 16:50:13 +0900

    First setup

---
+++ src/index.tsx
@@ -0,0 +1,6 @@
+/// <reference path="App.tsx" />
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+import App from "./App";
+
+ReactDOM.render(<App />, document.getElementById("root"));
---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+"use strict";
+
+const path = require("path");
+
+module.exports = {
+  // Set debugging source maps to be "inline" for
+  // simplicity and ease of use
+  mode: "development",
+  devtool: "inline-source-map",
+
+  // The application entry point
+  entry: path.resolve(__dirname, "src", "index.tsx"),
+
+  // Where to compile the bundle
+  // By default the output directory is `dist`
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js"
+  },
+
+  // Supported file loaders
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      }
+    ]
+  },
+  // $ means it must be .tsx
+  // File extensions to support resolving
+  resolve: {
+    extensions: [".ts", ".tsx", ".js", "json"]
+  }
+};
